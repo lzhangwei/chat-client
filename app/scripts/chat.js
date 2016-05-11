d7e4599b76e382069aa4cbdafa474c0a16bd3b0e
Zhang Wei <lzhangwei@outlook.com>
2016-05-12 00:28:06 +0800

    implement basic chat function in client

--- app/scripts/chat.js
+++ app/scripts/chat.js
@@ -1,15 +1,25 @@
 $(document).ready(function () {
   var userName;
   var userRole;
-  if (typeof(Storage) !== "undefined") {
-    userName = localStorage.getItem("login_name");
-    userRole = localStorage.getItem("user_role");
+  if (typeof(Storage) !== 'undefined') {
+    userName = localStorage.getItem('login_name');
+    userRole = localStorage.getItem('user_role');
     if (userName == null || userRole == null) {
-      alert("请先登录!");
+      alert('请先登录!');
       window.location.href = "login.html"
     } else {
       var socket = io();
-      
+      socket.emit('init', userName, userRole);
+
+      socket.on('message', function (message) {
+        console.log('---client get message---' + JSON.stringify(message));
+        $('#chat_panel').append('<li class="list-group-item">' + message.from + ': ' + message.message + '</li>');
+      });
     }
   }
+  $('#send_but').on('click', function () {
+    var message = $('#message_input').val();
+    $('#chat_panel').append('<li class="list-group-item">' + userName + ': ' + message + '</li>');
+    socket.emit('message', userName, userRole, message);
+  });
 });
